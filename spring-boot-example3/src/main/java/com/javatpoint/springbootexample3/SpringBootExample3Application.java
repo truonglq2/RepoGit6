fae2a4791b7c28003cedf47a811311c6b56e6c3b
truonglq2 <le.quang.truong2004@gmail.com>
2018-07-09 11:13:57 +0700

    Initial commit

---
+++ spring-boot-example3/src/main/java/com/javatpoint/springbootexample3/SpringBootExample3Application.java
@@ -0,0 +1,12 @@
+package com.javatpoint.springbootexample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample3Application.class, args);
+	}
+}
